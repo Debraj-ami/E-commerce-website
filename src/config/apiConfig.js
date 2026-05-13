f2bbcdf84c47604a8e9b1d45fdad3cc85206dc28
Debraj Mondal <debrajmondal@gmail.com>
2026-05-13 18:16:15 +0530

    change api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://ecommerce-server-production-b8f7.up.railway.app";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
