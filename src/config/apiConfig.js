d1540bfa7ccec30b5155b5edd12e051e51c729eb
Debraj Mondal <debrajmondal@gmail.com>
2026-05-13 21:12:25 +0530

    debug api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -9,6 +9,38 @@ export const api = axios.create({
     },
 });
 
+export const getUser = () => async (dispatch) => {
+
+  dispatch(getUserRequest());
+
+  try {
+
+    const jwt = localStorage.getItem("jwt");
+
+    // ADD THIS
+    console.log(`${API_BASE_URL}/api/users/profile`);
+
+    const response = await axios.get(
+      `${API_BASE_URL}/api/users/profile`,
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+
+    const user = response.data;
+
+    dispatch(getUserSuccess(user));
+
+  } catch (error) {
+
+    dispatch(getUserFailure(error.message));
+
+    console.log("get user error:", error);
+  }
+};
+
 // Add token dynamically before every request
 api.interceptors.request.use(
     (config) => {
